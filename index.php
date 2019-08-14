0e7b58b4aba19e26a29e852b5a6a9729019e3382
josiane <josianesoares587@gmail.com>
2019-08-14 16:06:44 -0600

    adicionando ícone email

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+include_once "footer.php"; 
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,14 +32,21 @@
           Assinaturas
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Como pedir</a>
-          <a class="dropdown-item" href="#">Entregas</a>
           <a class="dropdown-item" href="#">Cardápio do dia</a>
-          <a class="dropdown-item" href="#">Parcerias</a>
-          <a class="dropdown-item" href="#">Cadastrar Restaurante</a>
+          <a class="dropdown-item" href="#">Combos</a>
+          <a class="dropdown-item" href="#">Promoções</a>
       </li>
       <li class="nav-item">
         <a class="nav-link text-light" href="#" style="margin-left: 10px;">Fale Conosco</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="margin-left: 10px;">
+          Cadastro
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Cliente</a>
+          <a class="dropdown-item" href="#">Empresa</a>
+          <a class="dropdown-item" href="#">Restaurante</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -100,33 +113,6 @@
   </div>
  </div> 
 </content>
-
-  <div class="redes-sociais" style="text-align: center;">
-    <div class="container" style="margin-top: 65px;">
-      <img src="media/images/ico_2.jpg" alt="Rede Social 2" title="Rede Social 2">
-      <img src="media/images/ico_3.jpg" alt="Rede Social 3" title="Rede Social 3">
-      <img src="media/images/ico_4.jpg" alt="Rede Social 4" title="Rede Social 4">
-      <img src="media/images/ico_5.jpg" alt="Rede Social 5" title="Rede Social 5">
-    </div>
-  </div>
-
-  <footer>
-       <div class="footer-item">
-        <a href="caramba@deucerto.com">E-mail: caramba@deucerto.com</a>
-      </div>
-      <div class="footer-item">
-      Twitter: @quanta_gambiarra_meodeus
-      </div>
-      <div class="footer-item">
-        Facebook: Cheff Delivery
-      </div>
-      <div class="footer-item">
-        Instagram: @whoischeffdelivery_
-      </div>
-      <div class="footer-item">
-        <p>&copy; <strong>Mistura tudo que sai um site lindo</strong></p>
-      </div>
-  </footer>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
