7a9869f0fe79d2237edad2bcb4c3d57796c64c25
Felipe Schiocchet <goku.schio@gmail.com>
2019-08-14 16:27:13 -0300

    adicionei o número de telefone

--- CadastroEmpresa.php
+++ CadastroEmpresa.php
@@ -50,6 +50,10 @@ background-attachment: scroll;">
 						<option>Sociedade Anônima (SA)</option>
 					</select>
 				</div>
+						<div class="form-group">
+							<label for="telefone_cliente">Número de telefone</label>
+							<input type="text" class="form-control" id="telefone_cliente">
+						</div>
 				<div class="form-row">
 					<div class="form-group col-md-6">
 						<label for="cidade_empresa">Cidade</label>
@@ -72,12 +76,12 @@ background-attachment: scroll;">
 				</form>
 			</div>
 		</div>
-</div>
+	</div>
 </div>
 
-			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-		</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 
-		</html>
+</html>
