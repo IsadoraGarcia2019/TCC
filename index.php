4e6567d7ead2e04b443f46f461b5a0b8712ab138
josiane <josianesoares587@gmail.com>
2019-08-14 17:19:24 -0600

    alterando

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
 <head>
 	<meta charset="utf-8">
 	<title>Cheff Delivery</title>
-	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" type="text/css" href="media/css/estilos.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-image: url('media/images/img-fundo.jpg');">
     <header>
   
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #de2828">
