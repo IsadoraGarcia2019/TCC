70222006e080785d4b5cce9c7c008491fc55f1ac
josiane <josianesoares587@gmail.com>
2019-08-14 15:06:31 -0600

    alteração

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Cheff Delivery</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+  
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #de2828">
+    <img src="media/images/cheffmenu2.png" style="height: 65px; width: 150px">
+  
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link text-light" href="#" style="margin-left: 10px;">Página inicial <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="#" style="margin-left: 10px;">Sobre</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="margin-left: 10px;">
+          Assinaturas
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Como pedir</a>
+          <a class="dropdown-item" href="#">Entregas</a>
+          <a class="dropdown-item" href="#">Cardápio do dia</a>
+          <a class="dropdown-item" href="#">Parcerias</a>
+          <a class="dropdown-item" href="#">Cadastrar Restaurante</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="#" style="margin-left: 10px;">Fale Conosco</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Vai pedir o que?" aria-label="Search">
+      <button class="btn btn-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+  </div>
+</nav>
+</header>
+
+ <div id="container">  
+    <div class="box" style="background-color: #de2828">
+      <h1 class="titulo-box text-light">Promoções da semana</h1>
+      <div class="alert alert-success" role="alert">
+      <strong>Para você comer no café da manhã, almoço e jantar.</strong>
+    </div>
+  </div>
+
+  <content>
+    <div class="container">
+      <div class="conteudo-pagina">
+
+<div class="row">
+    <div class="col">
+        <div class="card" style="width: 20rem;">
+  <h5 class="card-header">Cardápio Massas</h5>
+  <div class="card-body">
+    <img src="media/images/massas.png" class="card-img-top" alt="..." style="height: 190px;">
+    <a href="#" class="btn btn-success" style="margin-top: 15px;">Vizualizar</a>
+  </div>
+</div>
+</div>
+
+<div class="col">
+        <div class="card" style="width: 20rem;">
+  <h5 class="card-header">Cardápio Sobremesas</h5>
+  <div class="card-body">
+    <img src="media/images/sobremesas.jpg" class="card-img-top" alt="..." style="height: 190px;">
+    <a href="#" class="btn btn-success" style="margin-top: 15px;">Vizualizar</a>
+  </div>
+</div>
+</div>
+
+<div class="row">
+    <div class="col" style="margin-top: 20px; margin-left: 15px;">
+        <div class="card" style="width: 20rem;">
+  <h5 class="card-header">Cardápio Vegetariano</h5>
+  <div class="card-body">
+    <img src="media/images/vegetariano.jpg" class="card-img-top" alt="..." style="height: 190px;">
+    <a href="#" class="btn btn-success" style="margin-top: 15px;">Vizualizar</a>
+  </div>
+</div>
+</div>
+
+<div class="col" style="margin-top: 20px; margin-left: 130px;">
+  <div class="card" style="width: 20rem;">
+  <h5 class="card-header">Cardápio Japonês</h5>
+  <div class="card-body">
+    <img src="media/images/sushi.jpg" class="card-img-top" alt="..." style="height: 190px;">
+    <a href="#" class="btn btn-success" style="margin-top: 15px;">Vizualizar</a>
+    </div>
+    </div>        
+   </div>
+  </div>
+ </div> 
+</content>
+
+  <div class="redes-sociais" style="text-align: center;">
+    <div class="container" style="margin-top: 65px;">
+      <img src="media/images/ico_2.jpg" alt="Rede Social 2" title="Rede Social 2">
+      <img src="media/images/ico_3.jpg" alt="Rede Social 3" title="Rede Social 3">
+      <img src="media/images/ico_4.jpg" alt="Rede Social 4" title="Rede Social 4">
+      <img src="media/images/ico_5.jpg" alt="Rede Social 5" title="Rede Social 5">
+    </div>
+  </div>
+
+  <footer>
+       <div class="footer-item">
+        <a href="caramba@deucerto.com">E-mail: caramba@deucerto.com</a>
+      </div>
+      <div class="footer-item">
+      Twitter: @quanta_gambiarra_meodeus
+      </div>
+      <div class="footer-item">
+        Facebook: Cheff Delivery
+      </div>
+      <div class="footer-item">
+        Instagram: @whoischeffdelivery_
+      </div>
+      <div class="footer-item">
+        <p>&copy; <strong>Mistura tudo que sai um site lindo</strong></p>
+      </div>
+  </footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
