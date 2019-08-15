9861aff39f516002720f0988ab5e7a210e3ebe22
josiane <josianesoares587@gmail.com>
2019-08-15 14:15:30 -0600

    mudei a foto de fundo

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
 	<link rel="stylesheet" type="text/css" href="media/css/estilos.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body style="background-image: url('media/images/img-fundo.jpg');">
+<body style="background-image: url('media/images/img-fundo2.jpg');
+             background-size: 100%;">
     <header>
   
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #de2828">
