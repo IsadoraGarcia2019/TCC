f4708c862cecc8d32a25928fabb8f87f6ad0e7a9
Isadora Zancanaro <isaaa349@gmail.com>
2019-08-15 15:13:18 -0300

    alterações necessárias

--- index.php
+++ index.php
@@ -10,22 +10,22 @@
 <body style="background-image: url('media/images/img-fundo2.jpg');
 background-size: 100%;">
 
-  <?php require_once "header.php"; ?>
+<?php require_once "header.php"; ?>
 
-  <div id="container">  
-    <div class="box" style="background-color: #de2828">
-      <h1 class="titulo-box text-light">Promoções da semana</h1>
-      <div class="alert alert-success" role="alert">
-        <strong>Para você comer no café da manhã, almoço e jantar.</strong>
-      </div>
+<div id="container">  
+  <div class="box" style="background-color: white; ">
+    <h1 class="titulo-box " style="color:#de2828"><strong>Promoções da semana</strong></h1>
+    <div class="alert alert-secondary" role="alert">
+      <strong>Para você comer no café da manhã, almoço e jantar.</strong>
     </div>
   </div>
+</div>
 
-  <?php require_once "footer.php"; ?>
+<?php require_once "footer.php"; ?>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
 
