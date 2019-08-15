5dbd98cc7a68304c722758b0995aaf3bed06d554
Isadora Zancanaro <isaaa349@gmail.com>
2019-08-15 17:10:34 -0300

    mais alterações

--- login.php
+++ login.php
@@ -4,46 +4,55 @@
 	<meta charset="utf-8">
 	<title>Login</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="media/css/estilos.css">
 
 </head>
-<body style="background-image: url('media/images/img-login.jpg');
-background-size: 98%;
-background-repeat: no-repeat;
-background-attachment: scroll;">
+<body style="background-color: rgb(235, 226, 225);">
 
-<div class="card col-md-4 offset-4 mt-5">
-	<div class="card-body"> 
-		<h1>Login</h1>
-		<br>
-		<form>
-			<div class="form-row">
-				<div class="col-md-12">
-					<label for="email"><strong>E-mail</strong></label>
-					<input type="email" class="form-control" id="email">
-				</div>
-				<div class="col-md-12">
-					<label for="senha"><strong>Senha</strong></label>
-					<input type="password" class="form-control" id="senha">
-				</div>
-			</div>
-			<div class="form-row">
-				<a href="" class="col-sm-12 col-md-12 text-center mt-3	" >Esqueci minha senha</a> 
-			</div>
-			<div class="form-row">
-				<button type="submit" class="btn btn-outline-secondary col-md-12 mb-3 mt-3" style="display:block;
-				margin-left: auto;
-				margin-right: auto;
-				width: 30%;">Entrar</button>
-				
-				<button type="submit" class="btn btn-outline-primary col-md-12 mb-3 mt-3" style="display:block;
-				margin-left: auto;
-				margin-right: auto;
-				width: 30%;">Criar uma nova conta</button>
-			</div>
-		</form>
+	<?php
+	require_once "header.php";
+	?>
+
+	<div class="row">
+		<div style="background-image: url('media/images/img-login.jpg'); min-height: 200px; min-width: 100%;     background-position: center 930px;
+		" class="shadow">
+		<div class="text-center my-5">
+			<h1 class="text-white">Login</h1>
+		</div> 
 	</div>
 </div>
+
+<div class="col-10 col-md-10 mt-5 offset-md-1" style="margin-top: -80px !important;">
+	<div class="card col-md-4 offset-4 mt-5">
+		<div class="card-body"> 
+			<form>
+				<div class="form-row">
+					<div class="col-md-12">
+						<label for="email"><strong>E-mail</strong></label>
+						<input type="email" class="form-control" id="email">
+					</div>
+					<div class="col-md-12">
+						<label for="senha"><strong>Senha</strong></label>
+						<input type="password" class="form-control" id="senha">
+					</div>
+				</div>
+				<div class="form-row">
+					<a href="#" class="col-sm-12 col-md-12 text-center text-danger mt-3" >Esqueci minha senha</a> 
+				</div>
+				<div class="form-row">
+					<button type="submit" class="btn btn-outline-secondary col-md-12 mb-3 mt-3">Entrar</button>
+
+					<button type="submit" class="btn btn-outline-danger col-md-12 mb-3 mt-3">Criar uma nova conta</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<?php
+	//inclusão do rodapé
+require_once "footer.php";
+?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
