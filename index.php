c788d9449e504f3739c6eeb16027d338492bbec0
josiane <josianesoares587@gmail.com>
2019-08-19 11:13:59 -0600

    alterando index

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
 <body style="background-image: url('media/images/food-pattern.png'); 
 background-size: 100%; background-color: rgb(235, 226, 225); background-repeat: no-repeat;">
 
-<!-- <body style="background-image: url('media/images/img-fundo2.jpg');
-  background-size: 100%;"> -->
-
   <?php require_once "header.php"; ?>
 
   <div id="container"> 
@@ -24,7 +21,6 @@ background-size: 100%; background-color: rgb(235, 226, 225); background-repeat: 
         </div>
       </div>
     </div>
-<<<<<<< HEAD
   </div>
 </div>
 
@@ -86,68 +82,9 @@ background-size: 100%; background-color: rgb(235, 226, 225); background-repeat: 
     <div class="card-body">
       <h3 class="card-title">Vegetariana</h3>
       <p class="card-text">E para os amantes de comida vegetariana, temos um cardápio especial para vocês</p>
-=======
-    <div class="card-index">
-      <div class="card-columns">
-        <div class="card shadow">
-          <img src="media/images/cafedamanha2.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Café da Manhã</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
-        </div>
-        <div class="card p-3 shadow">
-          <blockquote class="blockquote mb-0 card-body text-center text-danger">
-            <p><strong>Temos promoções todos os dias para melhor atender você, faça seu pedido e o frete é totalmente gratuito</strong></p>
-          </blockquote>
-        </div>
-        <div class="card shadow">
-          <img src="media/images/japonesa.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Asiática</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
-        </div>
-        <div class="card shadow">
-          <img src="media/images/frenchfries.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Almoço</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-        <div class="card p-3 text-danger text-center shadow">
-          <blockquote class="blockquote mb-0 card-body">
-            <p><strong>Receber sua comida em casa nunca foi tão fácil</strong></p>
-          </blockquote>
-        </div>
-        <div class="card shadow">
-          <img src="media/images/sobremesas.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Sobremesas</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-        <div class="card shadow">
-          <img src="media/images/massas1.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Jantar</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
-        </div>
-        <div class="card p-3 shadow">
-          <blockquote class="blockquote mb-0 card-body text-center text-danger">
-            <p><strong>Trabalhamos com pagamentos mensais onde você deposita quanto quer gastar</strong></p>
-          </blockquote>
-        </div>
-        <div class="card shadow">
-          <img src="media/images/vegetariano.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Vegetariana</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
           </div>
         </div>
       </div>
->>>>>>> d4adf5290d106da1b4800fae30db4938c0b36727
     </div>
   </div>
 
