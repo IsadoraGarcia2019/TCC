b4c40a450847724372add75c66baef0f3b7b0b17
Isadora Zancanaro <isaaa349@gmail.com>
2019-08-19 14:33:10 -0300

    alterações chatas

--- header.php
+++ header.php
@@ -2,6 +2,10 @@
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #de2828">
     <img src="media/images/cheffmenu2.png" style="height: 65px; width: 150px">
 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
