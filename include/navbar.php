7a01c91c9c3009393a1d5ee9621cf6a7a2cba6a0
Isadora Zancanaro <isaaa349@gmail.com>
2019-08-26 17:18:19 -0300

    bla

--- include/navbar.php
+++ include/navbar.php
@@ -1,4 +1,19 @@
+<?php
+
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>navbar</title>
+</head>
+<body>
+
 	<div>
 		Olá, <?=$_SESSION['nome_usuario']?>
 		<a href="backends/logoff.php" class="btn btn-dark ml-3">SAIR</a>
 	</div>
+
+</body>
+</html>
