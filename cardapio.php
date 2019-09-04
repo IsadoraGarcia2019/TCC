a2fcf45e235458eee547a8f1a127f19c461f66fb
João Victor <brasil_connectint@hotmail.com>
2019-09-04 17:00:43 -0300

    alterei o botao Criar uma nova conta

--- cardapio.php
+++ cardapio.php
@@ -27,7 +27,7 @@ require_once"head.php";
 		</div> 
 	</div>
 </div>
-<section class="menu-area section-gap" id="menu" style="background-color: rgb(235, 226, 225)!important; ">
+<section class="menu-area " id="menu" style="padding-top:120px;background-color: rgb(235, 226, 225)!important; ">
 	<div class="container">
 		<ul class="filter-wrap filters col-lg-12 no-padding">
 			<li class="active" data-filter="*">Todo menu</li>
@@ -463,11 +463,12 @@ require_once"head.php";
 			</div>
 		</div>
 	</div>
-</section>
 <?php
 //inclusão do rodapé
 require_once "footer.php";
 ?>
+</section>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
