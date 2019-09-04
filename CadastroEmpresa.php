2b3de045047d07b572d5bcda9a947eea15114450
João Victor <brasil_connectint@hotmail.com>
2019-09-04 14:23:29 -0300

    Teste

--- CadastroEmpresa.php
+++ CadastroEmpresa.php
@@ -17,7 +17,7 @@ if (isset($_COOKIE['alerta']) && !is_null($_COOKIE['alerta'])) {
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8">
-	<title>Cadastro Empresas</title>
+	<title>Cadastro Empresas TESTE</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="media/css/estilos.css">
 	<link rel="stylesheet" href="media/css/main.css">
