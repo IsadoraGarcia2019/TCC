d8d7cd104fa44c05f5d5da3e858364b38c7a605e
Isadora Zancanaro <isaaa349@gmail.com>
2019-09-20 17:06:20 -0300

    backends

--- vendas.php
+++ vendas.php
@@ -1,12 +1,12 @@
  <?php
 
-require_once"head.php";
+ require_once"head.php";
 
-require_once"include/conexao.php";
+ require_once"include/conexao.php";
 
-$lista = "SELECT * FROM finalizacao";
-$queryFinalizacao = mysqli_query($con, $lista);
-$lista = mysqli_fetch_all($queryFinalizacao, MYSQLI_ASSOC);
+ $lista = "SELECT * FROM finalizacao";
+ $queryFinalizacao = mysqli_query($con, $lista);
+ $lista = mysqli_fetch_all($queryFinalizacao, MYSQLI_ASSOC);
 
   // Verificar se existe alerta via COOKIE
  if (isset($_COOKIE['alerta']) && !is_null($_COOKIE['alerta'])) {
@@ -31,60 +31,48 @@ $lista = mysqli_fetch_all($queryFinalizacao, MYSQLI_ASSOC);
  	?>
 
  	<div class="row">
- 		<div class="shadow " style="  background-color: #1a1818	!important;
- 		background-image: url('media/images/food-pattern.png');
- 		min-height: 100px; 
- 		min-width: 100%; 
- 		background-size: 30%; 
- 		background-position: center 1050px;"> 
- 		<div class="text-center my-5">
- 			<h1 class="text-white mb-5">Vendas</h1>
- 		</div> 
+ 		<div class="shadow " style="background-color: #1a1818	!important;background-image: url('media/images/food-pattern.png');min-height: 100px; min-width: 100%; background-size: 30%; background-position: center 1050px;"> 
+ 			<div class="text-center my-5">
+ 				<h1 class="text-white mb-5">Vendas</h1>
+ 			</div> 
+ 		</div>
  	</div>
- </div>
 
-<div class="row">
+ 	<div class="row">
+ 		<?php foreach ($lista as $item) { ?>
+	 			<table class="table table-dark col-md-10 offset-md-1 mt-5" style="background-color: rgba(47, 47, 47);">
+ 				<thead>
+ 					<tr>
+ 						<th scope="col">Id</th>
+ 						<th scope="col">Data da compra</th>
+ 						<th scope="col">id do pacote</th>
+ 						<th scope="col">forma de pagamento</th>
+ 						<th scope="col">estado do pagamento</th>
+ 					</tr>
+ 				</thead>
+ 				<tbody>
+ 					<tr>
+ 						<td class=" text-danger"><?=utf8_encode($item['id_usuario'])?></td>
+ 						<td>19/09/2019</td>
+ 						<td><?=$item['id_pacote']?></td>
+ 						<td><?=$item['forma_pagamento']?></td>
+ 						<td><?=$item['status_pagamento']?></td>
+ 						<td><a class="btn btn-daek text-danger col-md-12" href="edicaoVendas.php?id=<?=$item['id']?>">Editar </a></td>
+ 					</tr>
+ 				</tbody>
+ 			</table>
+ 		<?php } ?>
+ 	</div>
 
-		<?php foreach ($lista as $item) { ?>
-
-
-			<table class="table table-borderless table-dark col-md-10 offset-md-1 mt-5">
-				<thead>
-					<tr>
-						<th scope="col">Id</th>
-						<th scope="col">Data da compra</th>
-						<th scope="col">id do pacote</th>
-						<th scope="col">forma de pagamento</th>
-						<th scope="col">estado do pagamento</th>
-						<th scope="col"><div><a class="btn btn-primary col-md-12 mt-3" href="edicaoVendas.php?id=<?=$item['id']?>">Editar Cardápio</a></div>
-</th>
-
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td class="card-title text-danger"><?=utf8_encode($item['id_usuario'])?></td>
-     					<td>19/09/2019</td>
-						<td><?=$item['id_pacote']?></td>
-						<td><?=$item['forma_pagamento']?></td>
-						<td><?=$item['status_pagamento']?></td>
-					</tr>
-					
-				</tbody>
-			</table>
-		<?php } ?>
-
-	</div>
- 			
- <?php
+ 	<?php
 //INCLUSÃO DO RODAPÉ
- require_once "footer.php";
- ?>
+ 	require_once "footer.php";
+ 	?>
 
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
+ 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+ 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ </body>
 
-</html>
+ </html>
 
