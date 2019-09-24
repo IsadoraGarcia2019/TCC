f1bb5c06545d27ea3f826f0076e2ca6030784ba9
Isadora Zancanaro <isaaa349@gmail.com>
2019-09-24 14:36:45 -0300

    mais alterações

--- cardapio.php
+++ cardapio.php
@@ -81,7 +81,7 @@ $lista = mysqli_fetch_all($queryCardapio, MYSQLI_ASSOC);
 								<div class="title-wrap d-flex justify-content-between">
 									<h4><?=utf8_encode($item['nome_comida'])?></h4>
 								</div>	
-										<?php if (isset($_SESSION['logado']) || $_SESSION['logado'] == true) { ?>
+								<?php if (isset($_SESSION['logado']) || $_SESSION['logado'] == true) { ?>
 									<?php if ($_SESSION['tp_usuario'] != 'administrador') { ?>
 										<a href="#" class="btn btn-outline-danger col-md-12">Pedir agora</a>
 									<?php } ?>
@@ -105,7 +105,7 @@ $lista = mysqli_fetch_all($queryCardapio, MYSQLI_ASSOC);
 								<div class="title-wrap d-flex justify-content-between">
 									<h4><?=utf8_encode($item['nome_comida'])?></h4>
 								</div>	
-										<?php if (isset($_SESSION['logado']) || $_SESSION['logado'] == true) { ?>
+								<?php if (isset($_SESSION['logado']) || $_SESSION['logado'] == true) { ?>
 									<?php if ($_SESSION['tp_usuario'] != 'administrador') { ?>
 										<a href="#" class="btn btn-outline-danger col-md-12">Pedir agora</a>
 									<?php } ?>
@@ -127,38 +127,9 @@ $lista = mysqli_fetch_all($queryCardapio, MYSQLI_ASSOC);
 		</div>
 	</div>
 </section>
-<center>
-	<div class="redes-sociais" style="background-color: #de2828;">
-		<div class="container">
 
-			<a href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRmTgXbBSZDhPSBJsPLwHzgBPqskCzpPtNTPhwSkgLdBBXnbmRScSHtKdnmsdqlgZFHsnsQ"><img src="media/images/email.png" alt="email" title="email" class="icones"></a>
+<?php require_once"footer.php";?>
 
-			<a href="https://www.instagram.com/deliiverycheff/?hl=pt-br"><img src="media/images/instagram.png" alt="instagram" title="instagram" id="instagram " class="icones"></a>
-
-<<<<<<< HEAD
-			<a href="https://twitter.com/delivery_cheff"><img src="media/images/twitter.png" alt="twitter" title="twitter" class="icones"></a>
-
-		</center>
-	</div>
-=======
-      <a href="https://twitter.com/delivery_cheff"><img src="media/images/twitter.png" alt="twitter" title="twitter" class="icones"></a>
-    </center>
-  </div>
->>>>>>> 1b04540a66ab8b47f49bf53b3f089c27d8603122
-</div>
-<footer>
-	<div class="footer-item">
-		<div class="row">
-			<a href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWsTJtrnhxXwfpDgvhvzGsLRlLgjHgjcfzcQFCTNqvbqkMSDXnPTwMNHPSkbjVnnrBWWPBfJq" class="col-sm-12 col-md-3">E-mail: deliiverycheff@gmail.com</a>
-			<a href="https://twitter.com/delivery_cheff" class="col-sm-12 col-md-3">Twitter: @delivery_cheff </a>
-			<a href="https://www.instagram.com/deliiverycheff/?hl=pt-br"class="col-sm-12 col-md-3">Instagram: @deliiverycheff</a>
-			<a href=""class="col-sm-12 col-md-3">Telefone: +55 (47) 3768-3352</a>
-		</div>
-	</div>
-	<div id="negrito" class="mt-4">
-		<p class="mt-5 font-italic">2019 &copy; <strong>Projeto desenvolvido por alunos da turma de PHP - Entra21/2019</strong></p>
-	</div>
-</footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
