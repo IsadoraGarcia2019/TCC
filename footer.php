112a884a4b481b29fae395694b540c336f721ffa
Isadora Zancanaro <isaaa349@gmail.com>
2019-09-24 16:51:49 -0300

    mais alterações

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-<center>
   <div class="redes-sociais" style="background-color: #de2828;">
     <div class="container" style="margin-top: 65px;">
 
@@ -8,21 +7,20 @@
 
       <a href="https://twitter.com/delivery_cheff"><img src="media/images/twitter.png" alt="twitter" title="twitter" class="icones"></a>
 
-    </center>
+    </div>
   </div>
-</div>
 
-<footer>
- <div class="footer-item">
-  <div class="row">
-    <a href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWsTJtrnhxXwfpDgvhvzGsLRlLgjHgjcfzcQFCTNqvbqkMSDXnPTwMNHPSkbjVnnrBWWPBfJq" class="col-sm-12 col-md-3">E-mail: deliiverycheff@gmail.com</a>
-    <a href="https://twitter.com/delivery_cheff" class="col-sm-12 col-md-3">Twitter: @delivery_cheff </a>
-    <a href="https://www.instagram.com/deliiverycheff/?hl=pt-br"class="col-sm-12 col-md-3">Instagram: @deliiverycheff</a>
-    <a href=""class="col-sm-12 col-md-3">Telefone: +55 (47) 3768-3352</a>
+  <footer>
+   <div class="footer-item">
+    <div class="row">
+      <a href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWsTJtrnhxXwfpDgvhvzGsLRlLgjHgjcfzcQFCTNqvbqkMSDXnPTwMNHPSkbjVnnrBWWPBfJq" class="col-sm-12 col-md-3">E-mail: deliiverycheff@gmail.com</a>
+      <a href="https://twitter.com/delivery_cheff" class="col-sm-12 col-md-3">Twitter: @delivery_cheff </a>
+      <a href="https://www.instagram.com/deliiverycheff/?hl=pt-br"class="col-sm-12 col-md-3">Instagram: @deliiverycheff</a>
+      <a href=""class="col-sm-12 col-md-3">Telefone: +55 (47) 3768-3352</a>
+    </div>
   </div>
-</div>
-<div id="negrito" class="mt-4">
-  <p class="mt-5 font-italic">2019 &copy; <strong>Projeto desenvolvido por alunos da turma de PHP - Entra21/2019</strong></p>
+  <div id="negrito" class="mt-4">
+    <p class="mt-5 font-italic">2019 &copy; <strong>Projeto desenvolvido por alunos da turma de PHP - Entra21/2019</strong></p>
 
-</div>
+  </div>
 </footer>
